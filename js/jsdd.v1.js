c2982fc43327d98642fe293420f4a1fe2da16a17
Al Rogatto <alrogattodev@gmail.com>
2024-09-20 13:01:11 -0300

    Design Updates and performance improvements

--- js/jsdd.v1.js
+++ js/jsdd.v1.js
@@ -1,29 +1,32 @@
 /******************
 Dump & Die for Javascript
-Version: 0.01.B - Always Beta
+Version: 0.02.B - Always Beta
 By Al Rogatto - al@rticons.com
 Based and Inspired by Binny V A(binnyva (at) hotmail (dot) com
-Date: 20/09/2024 - 1:13AM - Just managed to put my baby daughter to sleep
+Crated On: 18/05/2024 - 1:13AM - Just managed to put my baby daughter to sleep
+Last Update: 20/09/2024
 Objective: Dump all the content of an array, object or variable
 ******************************************************************************/
-function dd(theArray,level) {
-    let ddText = "";
-    if(!level) level = 0;
+function dd(theArray, level = 0) {
+    let result = [];
+    const levelPadding = ' '.repeat((level + 1) * 5);
 
-    var levelPadding = "";
-    for (let j=0; j<level+1; j++) levelPadding += "     ";
-    if(typeof(theArray) == 'object') {
-        for(let item in theArray) {
-            let value = theArray[item];
-            if(typeof(value) == 'object') {
-                ddText += levelPadding + "'" + item + "'___\n";
-                ddText += dd(value,level+1);
-            } else {
-                ddText += levelPadding  + "'" + item + "' => \"" + value + "\"\n";
+    if (typeof theArray === 'object') {
+        for (let item in theArray) {
+            if (theArray.hasOwnProperty(item)) {
+                let value = theArray[item];
+                if (typeof value === 'object') {
+                    result.push(`${levelPadding}'${item}'{\n`);
+                    result.push(dd(value, level + 1));
+                } else {
+                    result.push(`${levelPadding}'${item}' => "${value}"\n`);
+                }
             }
         }
-    } else { 
-        ddText = "====>"+ theArray+"<===("+typeof(arr)+")";
+    } else {
+        return `====>${theArray}<===(${typeof theArray})`;
     }
-    return ddText;
+
+    return result.join('');
 }
+
