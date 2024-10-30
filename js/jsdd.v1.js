52966d7090e5799fe33a801e72edd11dff5a8211
Al Rogatto <alrogattodev@gmail.com>
2024-10-30 12:17:25 -0300

    Enhanced with die()

--- js/jsdd.v1.js
+++ js/jsdd.v1.js
@@ -1,11 +1,11 @@
 /******************
 Dump & Die for Javascript
-Version: 0.02.B - Always Beta
+Version: 0.03 - Enhanced with Die
 By Al Rogatto - al@rticons.com
-Based and Inspired by Binny V A(binnyva (at) hotmail (dot) com
-Crated On: 18/05/2024 - 1:13AM - Just managed to put my baby daughter to sleep
+Based and Inspired by Binny V A (binnyva (at) hotmail (dot) com)
+Created On: 18/05/2024 - 1:13AM - Just managed to put my baby daughter to sleep
 Last Update: 20/09/2024
-Objective: Dump all the content of an array, object or variable
+Objective: Dump all the content of an array, object or variable and halt execution
 ******************************************************************************/
 function dd(theArray, level = 0) {
     let result = [];
@@ -16,17 +16,18 @@ function dd(theArray, level = 0) {
             if (theArray.hasOwnProperty(item)) {
                 let value = theArray[item];
                 if (typeof value === 'object') {
-                    result.push(`${levelPadding}'${item}'{\n`);
+                    result.push(`${levelPadding}'${item}' {\n`);
                     result.push(dd(value, level + 1));
+                    result.push(`${levelPadding}}\n`);
                 } else {
                     result.push(`${levelPadding}'${item}' => "${value}"\n`);
                 }
             }
         }
     } else {
-        return `====>${theArray}<===(${typeof theArray})`;
+        result.push(`====> ${theArray} <=== (${typeof theArray})`);
     }
 
-    return result.join('');
+    console.log(result.join(''));
+    throw new Error('Execution halted by dd()');
 }
-
