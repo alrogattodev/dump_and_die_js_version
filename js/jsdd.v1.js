60e4c54ccef512a6b4e2c11fb618a793be86d23a
Al Rogatto <alrogattodev@gmail.com>
2024-10-30 12:04:48 -0300

    Enhanced die() function

--- js/jsdd.v1.js
+++ js/jsdd.v1.js
@@ -1,29 +1,33 @@
 /******************
 Dump & Die for Javascript
-Version: 0.01.B - Always Beta
+Version: 0.03 - Enhanced with Die
 By Al Rogatto - al@rticons.com
-Based and Inspired by Binny V A(binnyva (at) hotmail (dot) com
-Date: 20/09/2024 - 1:13AM - Just managed to put my baby daughter to sleep
-Objective: Dump all the content of an array, object or variable
+Based and Inspired by Binny V A (binnyva (at) hotmail (dot) com)
+Created On: 18/05/2024 - 1:13AM - Just managed to put my baby daughter to sleep
+Last Update: 30/10/2024
+Objective: Dump all the content of an array, object or variable and halt execution
 ******************************************************************************/
-function dd(theArray,level) {
-    let ddText = "";
-    if(!level) level = 0;
+function dd(theArray, level = 0) {
+    let result = [];
+    const levelPadding = ' '.repeat((level + 1) * 5);
 
-    var levelPadding = "";
-    for (let j=0; j<level+1; j++) levelPadding += "     ";
-    if(typeof(theArray) == 'object') {
-        for(let item in theArray) {
-            let value = theArray[item];
-            if(typeof(value) == 'object') {
-                ddText += levelPadding + "'" + item + "'___\n";
-                ddText += dd(value,level+1);
-            } else {
-                ddText += levelPadding  + "'" + item + "' => \"" + value + "\"\n";
+    if (typeof theArray === 'object') {
+        for (let item in theArray) {
+            if (theArray.hasOwnProperty(item)) {
+                let value = theArray[item];
+                if (typeof value === 'object') {
+                    result.push(`${levelPadding}'${item}' {\n`);
+                    result.push(dd(value, level + 1));
+                    result.push(`${levelPadding}}\n`);
+                } else {
+                    result.push(`${levelPadding}'${item}' => "${value}"\n`);
+                }
             }
         }
-    } else { 
-        ddText = "====>"+ theArray+"<===("+typeof(arr)+")";
+    } else {
+        result.push(`====> ${theArray} <=== (${typeof theArray})`);
     }
-    return ddText;
+
+    console.log(result.join(''));
+    throw new Error('Execution halted by dd()');
 }
